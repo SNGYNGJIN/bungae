1623ac0a936b2961abeb98ff16cf7459912ea40c
Jin <jsy9508@gmail.com>
2024-04-29 09:43:16 +0900

    init commit

---
+++ src/main/java/com/multi/bungae/BungaeApplication.java
@@ -0,0 +1,13 @@
+package com.multi.bungae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BungaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BungaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multi/bungae/BungaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.bungae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BungaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
