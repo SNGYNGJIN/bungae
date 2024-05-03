288060f9899890c29e8fccdd232968fc075c432e
LeeYongJeon <dydwjs7942@naver.com>
2024-05-03 13:02:20 +0900

    Feat: 지도 불러오기, 내 위치 표현

--- src/main/java/com/multi/bungae/BungaeApplication.java
+++ src/main/java/com/multi/bungae/BungaeApplication.java
@@ -2,12 +2,11 @@ package com.multi.bungae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class BungaeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BungaeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BungaeApplication.class, args);
+    }
 }
